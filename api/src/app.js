e283396f13139ba106d8677df6281e2d1f9079e0
Evgenii <7zhhovalyg@mail.ru>
2026-04-23 09:49:02 +0300

    fix error with cors

--- api/src/app.js
+++ api/src/app.js
@@ -12,7 +12,6 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 )
-app.options("*", cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
